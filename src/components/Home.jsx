aad1a689f572ad2219e3e6feed1a66f8ff2675f2
MansiJoshi1701 <mansijoshi.mj1798@gmail.com>
2025-07-23 15:23:02 -0700

    New folder/file feature added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,17 +2,51 @@ import { useState } from "react";
 
 function Home({ data }) {
 
-    console.log(data);
     const [expand , setExpand] = useState(false);
+    const [showInput , setShowInput] = useState({
+        isVisible: false,
+        isFolder: null
+    });
+
+    const handleClick = (e , isFolder) => {
+
+        e.stopPropagation();
+        setShowInput({
+            isVisible: true,
+            isFolder
+        });
+        //setExpand(true);
+    }
     
     if(data.isFolder) {
         return(
             <div style={ { marginTop: 5 }}>
                 <div className="folder" onClick={() => setExpand(!expand)}>
                     <span>📁 {data.name}</span>
+                    <div>
+                        <button onClick={(e) => handleClick(e , true)}>Folder +</button>
+                        <button onClick={(e) => handleClick(e , false)}>File +</button>
+                    </div>
+            
                 </div>
 
+                {
+                    showInput.isVisible && (
+                        <div className="inputContainer" style={{paddingLeft: 25}}>
+                            <span>{showInput.isFolder ? "📁" : "📃"}</span>
+                            <input 
+                                type="text"
+                                className="inputContainer_input" 
+                                autoFocus
+                                onBlur={() => setShowInput({...showInput , isVisible: false})}
+                            />
+                        </div>
+                    )
+                }
+
                 <div style={{display: expand ? "block" : "none" , paddingLeft: 25}}>
+                    
+
                     {data.children.map((folder) => {
                         return(
                             <Home data={folder} key={folder.id}/> //This is how to display recursively
